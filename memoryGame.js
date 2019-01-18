e7ccc93aec814cf0fcc545c84ea485ea34036d51
Jimmy Svensson <jimmy888swe@gmail.com>
2019-01-18 20:35:27 +0100

    flip

--- memoryGame.js
+++ memoryGame.js
@@ -7,56 +7,68 @@ var first;
 var second;
 var match = 0;
 
-// An array with images
+// array med images
 
 
-var cards = [
+var kort = [
     {
         img: 'img/gris.png',
+        img: 'img/back_side.png',
         id: 'A'
     },
     {
         img: 'img/hast.png',
+        img: 'img/back_side.png',
         id: 'B'
     },
     {
         img: 'img/kalkon.png',
+        i: 'img/back_side.png',
         id: 'C'
     },
     {
         img: 'img/ko.png',
+        i: 'img/back_side.png',
         id: 'D'
     },
     {
         img: 'img/lamm.png',
+        i: 'img/back_side.png',
         id: 'E'
     },
     {
         img: 'img/tupp.png',
+        i: 'img/back_side.png',
         id: 'F'
     },
     {
         img: 'img/gris.png',
+        i: 'img/back_side.png',
         id: 'A'
     },
     {
         img: 'img/hast.png',
+        i: 'img/back_side.png',
         id: 'B'
     },
     {
         img: 'img/kalkon.png',
+        i: 'img/back_side.png',
         id: 'C'
     },
     {
         img: 'img/ko.png',
+        i: 'img/back_side.png',
         id: 'D'
     },
     {
         img: 'img/lamm.png',
+        i: 'img/back_side.png',
         id: 'E'
     },
     {
         img: 'img/tupp.png',
+        i: 'img/back_side.png',
         id: 'F'
     }
 ];         
@@ -70,17 +82,42 @@ function shuffle(a) {
     }
     return a;
 }
-shuffle(cards);
+shuffle(kort);
 
 var container = document.getElementById('container');
 var cardContainer = document.createElement('section');
 container.appendChild(cardContainer);
 
+// skriver ut array med bilder
 
-cards.forEach(obj => {
+
+var sheet = document.createElement('style')
+sheet.innerHTML = "img {border: 2px solid black;  background-image: url(/img/back_side.png);}";
+document.body.appendChild(sheet);
+
+
+kort.forEach(obj => {
     var cardImg = document.createElement('img');
     cardImg.id = obj.id;
     cardImg.src = obj.img;
     cardContainer.appendChild(cardImg);
 });
+
+
+
+/*
+cards.forEach(obj => {
+    var cardImg = document.createElement('img');
+    cardImg.id = obj.id;
+    cardImg.src = obj.i;
+    cardContainer.appendChild(cardImg);
+}); */
+
+const cards = document.querySelectorAll('img');
+
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(cards => cards.addEventListener('click', flipCard));
 
