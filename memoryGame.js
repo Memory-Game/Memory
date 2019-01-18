f25c2b2451ec4251778caae768488a5a5fff386d
Jimmy Svensson <jimmy888swe@gmail.com>
2019-01-18 13:07:36 +0100

    shuffle

--- memoryGame.js
+++ memoryGame.js
@@ -8,6 +8,8 @@ var second;
 var match = 0;
 
 // An array with images
+
+
 var cards = [
     {
         img: 'img/gris.png',
@@ -57,12 +59,23 @@ var cards = [
         img: 'img/tupp.png',
         id: 'F'
     }
-];                   
+];         
 
+// shuffle
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+shuffle(cards);
 
 var container = document.getElementById('container');
 var cardContainer = document.createElement('section');
 container.appendChild(cardContainer);
+
 
 cards.forEach(obj => {
     var cardImg = document.createElement('img');
