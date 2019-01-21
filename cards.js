7251b1423148c70fe730e7a17d0bdd374e272d20
Jimmy Svensson <jimmy888swe@gmail.com>
2019-01-21 18:26:10 +0100

    ImFuckingAwesome

--- cards.js
+++ cards.js
@@ -2,32 +2,24 @@ const play = document.querySelector(".memory-game");
 var i;
 
 var playCards = ['hast', 'lamm', 'tupp', 'kalkon', 'ko','gris','hast', 'lamm', 'tupp', 'kalkon', 'ko','gris' ]
-/*
-for (var i = 0, len = playCards.length; i < len; i++) {
-    
-    content = ` 
-    <div class="memoryCard" data-framework="${this.playCards[i]}">
-    <img class="front-face" src="./img/${this.playCards[i]}.png" alt="${this.playCards[i]}">
-    <img class="back-face" src="./img/back_side.png" alt="Memory Card">
-    </div> 
-    `;
-}; */
+
 
 for (i=0;i<playCards.length;i++){
     console.log(playCards[i]);
     
     
     playCards.forEach( function (){
-        content = ` 
-        <div class="memoryCard" data-framework="${this.playCards[i]}">
+        let content= document.createElement('div');
+        content.innerHTML = ` 
+        
         <img class="front-face" src="./img/${this.playCards[i]}.png" alt="${this.playCards[i]}">
         <img class="back-face" src="./img/back_side.png" alt="Memory Card">
         </div> 
         `;
+        content.classList.add('memoryCard');
+        play.appendChild(content);
     })
     
     
 };
-play.innerHTML = content;
- 
     
