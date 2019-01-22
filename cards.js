5779b0bb802fa952357b428c3ac6f7b3bd0d35bb
Jimmy Svensson <jimmy888swe@gmail.com>
2019-01-22 10:46:15 +0100

    updatedCards

--- cards.js
+++ cards.js
@@ -1,25 +1,103 @@
+
+
+/*
 const play = document.querySelector(".memory-game");
 var i;
 
-var playCards = ['hast', 'lamm', 'tupp', 'kalkon', 'ko','gris','hast', 'lamm', 'tupp', 'kalkon', 'ko','gris' ]
-
+var playCards = ['gris', 'ko', 'lamm', 'kalkon', 'tupp', 'hast','gris', 'ko', 'lamm', 'kalkon', 'tupp', 'hast']
+  
 
 
 
 for (i=0;i<playCards.length;i++){
     console.log(playCards[i]);
-  
+ // playCards.forEach( function (){
         let content= document.createElement('div');
          content.innerHTML = ` 
-        <img class="front-face" src="./img/${this.playCards[i]}.png" alt="${this.playCards[i]}">
+
+        <img class="front-face" src="./img/${playCards[i]}.png" dataset-framework="${playCards[i]}" alt="${playCards[i]}">
         <img class="back-face" src="./img/back_side.png" alt="Memory Card">
         
         `;
         content.classList.add('memoryCard');
-        content.classList.add(`data-framework="${this.playCards[i]}">`);
         play.appendChild(content);
-
+   // })
 };
+    */
+    
+const play = document.querySelector(".memory-game");
+var i;
+
+var playCards = [
+    {
+        id: 1,
+    img: 'ko'
+} ,
+{
+    id: 2,
+    img: 'hast'
+} ,
+{
+    id: 3,
+    img: 'gris'
+} ,
+{
+    id: 4,
+    img: 'lamm'
+} ,
+{
+    id: 5,
+    img: 'kalkon'
+} ,
+{
+    id: 6,
+    img: 'tupp'
+} ,
+{
+    id: 1,
+img: 'ko'
+} ,
+{
+id: 2,
+img: 'hast'
+} ,
+{
+id: 3,
+img: 'gris'
+} ,
+{
+id: 4,
+img: 'lamm'
+} ,
+{
+id: 5,
+img: 'kalkon'
+} ,
+{
+id: 6,
+img: 'tupp'
+} 
+]
+
+
+
+
+//for (i=0;i<playCards.length;i++){
+  //  console.log(playCards.id);
+  playCards.forEach( function (playCards){
+        let content= document.createElement('div');
+         content.innerHTML = ` 
+
+        <img class="front-face" src="./img/${playCards.img}.png" id='${playCards.id}' alt="${playCards.img}">
+        <img class="back-face" src="./img/back_side.png" alt="Memory Card">
+        
+        `;
+        content.classList.add('memoryCard');
+        play.appendChild(content);
+    })
+//};
+    
+    
     
     
     
--- memoryGame.js
+++ memoryGame.js
@@ -11,6 +11,78 @@ var lockBoard = false;
 var firstCard, secondCard;
 var move = 0;
 var points = 0;
+//-----
+
+    
+const play = document.querySelector(".memory-game");
+var i;
+
+var playCards = [
+    {
+        id: 1,
+    img: 'ko'
+} ,
+{
+    id: 2,
+    img: 'hast'
+} ,
+{
+    id: 3,
+    img: 'gris'
+} ,
+{
+    id: 4,
+    img: 'lamm'
+} ,
+{
+    id: 5,
+    img: 'kalkon'
+} ,
+{
+    id: 6,
+    img: 'tupp'
+} ,
+{
+    id: 1,
+img: 'ko'
+} ,
+{
+id: 2,
+img: 'hast'
+} ,
+{
+id: 3,
+img: 'gris'
+} ,
+{
+id: 4,
+img: 'lamm'
+} ,
+{
+id: 5,
+img: 'kalkon'
+} ,
+{
+id: 6,
+img: 'tupp'
+} 
+]
+
+
+playCards.forEach( function (playCards){
+  let content= document.createElement('div');
+   content.innerHTML = ` 
+
+  <img class="front-face" src="./img/${playCards.img}.png" id='${playCards.id}' alt="${playCards.img}">
+  <img class="back-face" src="./img/back_side.png" alt="Memory Card">
+  
+  `;
+  content.classList.add('memoryCard');
+  play.appendChild(content);
+})
+
+
+//-----
 
 // starta/ starta om 0-ställer   
 function start(){
@@ -49,7 +121,7 @@ function flipCard() {
 }
 // kollar om dataset matchar, gör dom de tas klick bort från korten med flip aktiv, är det inte match, tas flip bort med 1 sekunds fördröjning. 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch = firstCard.id === secondCard.id;
 
   isMatch ? disableCards() : unflipCards();
 }
